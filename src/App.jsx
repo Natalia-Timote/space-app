d03dbba412943e4cc62f40200bed98e3577b8ba5
Natalia Timote <nataliamirian.nt@gmail.com>
2025-04-19 00:06:00 -0300

    estruturando a página e início da criação da galeria

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import EstilosGlobais from './components/EstilosGlobais';
 import Cabecalho from './components/Cabecalho';
 import BarraLateral from './components/BarraLateral';
 import Banner from './components/TituloEstilizado';
+import Galeria from './components/Galeria';
 
 const FundoGradiente = styled.div`
   background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
@@ -10,13 +11,37 @@ const FundoGradiente = styled.div`
   min-height: 100vh;
 `;
 
+const AppContainer = styled.div`
+  width: 1440px;
+  margin: 0 auto;
+  max-width: 100%;
+`;
+
+const MainContainer = styled.main`
+  display: flex;
+  gap: 24px;
+`;
+
+const ConteudoGaleria = styled.section`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+`;
+
 function App() {
   return (
     <FundoGradiente>
       <EstilosGlobais />
-      <Cabecalho />
-      <BarraLateral />
-      <Banner $backgroundImage={"src/assets/banner.png"} texto={"A galeria mais completa de fotos do espaço!"} />
+      <AppContainer>
+        <Cabecalho />
+        <MainContainer>
+          <BarraLateral />
+          <ConteudoGaleria>
+            <Banner $backgroundImage={"src/assets/banner.png"} texto={"A galeria mais completa de fotos do espaço!"} />
+            <Galeria />
+          </ConteudoGaleria>
+        </MainContainer>
+      </AppContainer>
     </FundoGradiente>
 
   )
---
+++ src/components/Galeria/index.jsx
@@ -0,0 +1,7 @@
+const Galeria = () => {
+    return (
+        <h2>Navegue pla galeria</h2>
+    )
+}
+
+export default Galeria;
